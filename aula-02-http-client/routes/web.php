a817c5a25f5d27a5d49c1fe385de6d97c5b6d78d
EleazarSF <eleazarsf@gmail.com>
2022-06-28 09:04:47 -0300

    aula 02 - http - client

---
+++ aula-02-http-client/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViaCepController;
+
+Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
